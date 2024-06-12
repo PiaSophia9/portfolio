08c09c7216ef57680124104f9deae614c16a57b8
Sophia Brouwers <sophia.brouwers@gmx.de>
2024-06-12 14:48:13 +0200

    responsive nearly finished

--- src/app/main-page/portfolio/project/project.component.ts
+++ src/app/main-page/portfolio/project/project.component.ts
@@ -18,6 +18,7 @@ export class ProjectComponent {
     title: string;
     technologies: string[];
     description: string;
+    beschreibung: string;
     linkLiveTest: string;
     linkGithub: string;
     isReversed: boolean;
@@ -28,6 +29,8 @@ export class ProjectComponent {
       technologies: ['JavaScript | HTML | CSS'],
       description:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
+      beschreibung:
+        'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
       linkLiveTest: '',
       linkGithub: 'https://github.com/PiaSophia9/join_2.0',
       isReversed: false,
@@ -38,20 +41,24 @@ export class ProjectComponent {
       technologies: ['JavaScript | HTML | CSS'],
       description:
         'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the giant Chicken.',
+      beschreibung:
+        'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
       linkLiveTest: '',
       linkGithub: 'https://github.com/PiaSophia9/pollo_loco',
       isReversed: true,
     },
-    {
-      imageSrc: './assets/img/portfolio/laptop.svg',
-      title: 'Pokedex',
-      technologies: ['JavaScript | HTML | CSS'],
-      description:
-        'A Slack clone app that facilitates smooth team communication and collaboration through messaging, file sharing, and customizable channels to enhance workplace efficiency.',
-      linkLiveTest: '',
-      linkGithub: 'https://github.com/PiaSophia9/pokedex',
-      isReversed: false,
-    },
+    // {
+    //   imageSrc: './assets/img/portfolio/laptop.svg',
+    //   title: 'Pokedex',
+    //   technologies: ['JavaScript | HTML | CSS'],
+    //   description:
+    //     'A Slack clone app that facilitates smooth team communication and collaboration through messaging, file sharing, and customizable channels to enhance workplace efficiency.',
+    //   beschreibung:
+    //     'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
+    //   linkLiveTest: '',
+    //   linkGithub: 'https://github.com/PiaSophia9/pokedex',
+    //   isReversed: false,
+    // },
   ];
 
   project!: {
