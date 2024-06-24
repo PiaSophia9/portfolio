6acd2d57aec8e4062eb68992835aeb9c57d11507
Sophia Brouwers <sophia.brouwers@gmx.de>
2024-06-24 14:32:22 +0200

    deleted not needed comments

--- src/app/main-page/portfolio/project/project.component.ts
+++ src/app/main-page/portfolio/project/project.component.ts
@@ -14,40 +14,4 @@ import { ProjectInfoService } from '../../../service/project-info/project-info.s
 export class ProjectComponent {
   translate = inject(TranslationService);
   projectInfo = inject(ProjectInfoService);
-
-  // projects: {
-  //   imageSrc: string;
-  //   title: string;
-  //   technologies: string[];
-  //   description: string;
-  //   beschreibung: string;
-  //   linkLiveTest: string;
-  //   linkGithub: string;
-  //   isReversed: boolean;
-  // }[] = [
-  //   {
-  //     imageSrc: './assets/img/portfolio/laptop_join.png',
-  //     title: 'Join',
-  //     technologies: ['JavaScript | HTML | CSS'],
-  //     description:
-  //       'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.',
-  //     beschreibung:
-  //       'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
-  //     linkLiveTest: 'https://sophia-brouwers.de/join/',
-  //     linkGithub: 'https://github.com/PiaSophia9/join_2.0',
-  //     isReversed: false,
-  //   },
-  //   {
-  //     imageSrc: './assets/img/portfolio/laptop_pollo_loco.png',
-  //     title: 'El Pollo Loco',
-  //     technologies: ['JavaScript | HTML | CSS'],
-  //     description:
-  //       'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against the giant Chicken.',
-  //     beschreibung:
-  //       'Task-Manager inspiriert vom Kanban-System. Erstellen und organisieren Sie Aufgaben mit Drag-and-Drop-Funktionen, weisen Sie Benutzer und Kategorien zu.',
-  //     linkLiveTest: 'https://sophia-brouwers.de/el-pollo-loco/',
-  //     linkGithub: 'https://github.com/PiaSophia9/pollo_loco',
-  //     isReversed: true,
-  //   },
-  // ];
 }
